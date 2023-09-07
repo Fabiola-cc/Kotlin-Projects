f0ecb17fd1c3dd9c568360359ceddc41013541f7
Fabiola Contreras <fcontrerascol@gmail.com>
2023-09-07 08:48:25 -0600

    File creation

---
+++ laboratorio6/app/src/main/java/com/example/laboratorio6/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package com.example.laboratorio6.ui.theme
+
+import android.app.Activity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+
+private val LightColorScheme = lightColorScheme(
+    primary = Tangerine,
+    secondary = Coral,
+    tertiary = Apricot,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun Laboratorio6Theme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = LightColorScheme
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
